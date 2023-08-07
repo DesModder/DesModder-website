0611c10f7c117eb60478f363aa6773d140857e56
Jared Hughes <jahughes241@gmail.com>
2023-08-06 23:45:19 -0700

    Add favicons

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,9 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/css");
   eleventyConfig.addWatchTarget("./src/css/");
+  eleventyConfig.addPassthroughCopy({
+    "./img/favicon/*.{png,ico,webmanifest}": "/",
+  });
   return {
     dir: {
       input: "src",
---
+++ img/favicon/README.md
@@ -0,0 +1,2 @@
+1. Take https://github.com/DesModder/DesModder/blob/main/assets/desmodder.png
+2. Send it to https://favicon.io/favicon-converter/
